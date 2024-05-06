ca7baf9eb09c060cb977faf2decadedaa357ef4e
MichalJanikPrz <wmicjan86@gmail.com>
2024-05-06 20:11:35 +0200

    Calendar View #1

--- front/src/App.js
+++ front/src/App.js
@@ -6,11 +6,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Main from './Component/Main';
 import RemindPassword from './Component/RemindPassword';
 
+import Calendar from './Component/Calendar';
+import TeamStatistic from './Component/TeamStatistic';
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          
+          <Route path='calendar/' Component={Calendar} />
+          <Route path='teamstatistic/' Component={TeamStatistic} />
+
           <Route path='/' Component={Menu} />
           <Route path='login/' Component={Login} />
           <Route path='reset_password/:token' Component={ResetPassword} />
---
+++ front/src/Component/Calendar.js
@@ -0,0 +1,62 @@
+import '../Style/Calendar.css'
+
+const Calendar = () => {    
+    return (
+        <div className="calendar">
+            <p>March</p>
+            <table className="table_days">
+                <tr>
+                    <td className="week">03</td>
+                        
+                    <div className="day">
+                        <td>Mo</td><td>Tu</td><td>We</td><td>Th</td><td>Fr</td><td>Sa</td><td>Su</td>
+                    </div>
+                </tr>
+                <tr>
+                    <td className="week">9</td>
+                        
+                    <div className="days">
+                        <td>26</td><td>27</td><td>28</td><td>29</td><td>1</td><td>2</td><td>3</td>
+                    </div>
+                </tr>
+                <tr>
+                    <td className="week">10</td>
+
+                    <div className="days">
+                        <td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td>
+                    </div>
+                </tr>
+                <tr>
+                    <td className="week">11</td>
+                            
+                    <div className="days">
+                        <td>11</td><td>12</td><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td>
+                    </div>
+                </tr>
+                <tr>
+                    <td className="week">12</td>
+                            
+                    <div className="days">
+                        <td>18</td><td>19</td><td>20</td><td>21</td><td>22</td><td>23</td><td>24</td>
+                    </div>
+                </tr>
+                <tr>
+                    <td className="week">13</td>
+                            
+                    <div className="days">
+                        <td>25</td><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>31</td>
+                    </div>
+                </tr>
+                <tr>
+                    <td className="week">14</td>
+
+                    <div className="days">
+                        <td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td>
+                    </div>
+                </tr>
+            </table>
+        </div>
+    );
+}
+ 
+export default Calendar;
