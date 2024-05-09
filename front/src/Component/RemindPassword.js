394df1400a323e5ad4795d81c9234d130f43c1b9
1snajper <tuleckikacper@gmail.com>
2024-05-09 18:48:07 +0200

    Added additional information about state of the sent email

--- front/src/Component/RemindPassword.js
+++ front/src/Component/RemindPassword.js
@@ -9,7 +9,7 @@ import { useState } from 'react';
 const RemindPassword = () => {
 
     const [email,setEmail] = useState('')
-
+    const [status,setStatus]=useState(0);
     const sendEmail = (event) =>{
         event.preventDefault();
         fetch('http://localhost:8184/auth/remind_password/',{
@@ -17,27 +17,58 @@ const RemindPassword = () => {
             method: 'POST',
             headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin":"allow"},
             body: JSON.stringify({email:email})
-        })
+        }).then(setStatus(1)).catch(setStatus(2));
     }
-    
-    return ( 
-        <div>
+    if(status===0)
+    {
+        return ( 
             <div>
-                <form id="formPrzypomnij">
-                    <div id="text">Przypomnij hasło:</div>
-                        <div id="idPrzypomnij">
-                            
+                <div>
+                    <form id="formPrzypomnij">
+                        <div id="text">Przypomnij hasło:</div>
+                        <div id="idPrzypomnij">                            
                                 <div className="inputfield">
                                     <label className="topic">Email</label>
                                     <input type="email" className="data" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
                                 </div>
-                                <input className="przypomnij" type="submit" value="Przypomnij" onClick={sendEmail}/>
-                            
                         </div>
+                        <input className="przypomnij" type="submit" value="Przypomnij" onClick={sendEmail}/>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+    else if(status===1)
+    {
+        return (
+            <div> 
+                <div id="formPrzypomnij">
+                    <form id="emailsuc">
+                        <label id="informacja1">Udało się wysłać emaila</label>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+    else if(status===2)
+    {
+        return (
+            <div>
+                <form id="formPrzypomnij2">
+                <div id="text">Przypomnij hasło:</div>
+                        <div id="idPrzypomnij2">    
+                            <label id="informacja2">Nie udało się wyślać emaila<br/>Spróbuj ponownie</label>                        
+                                <div className="inputfield">
+                                    <label className="topic">Email</label>
+                                    <input type="email" className="data" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+                                </div>
+                        </div>
+                        <input className="przypomnij" type="submit" value="Przypomnij" onClick={sendEmail}/>
+                    
                 </form>
             </div>
-        </div>
-    );
+        );
+    }
 }
  
 export default RemindPassword;
