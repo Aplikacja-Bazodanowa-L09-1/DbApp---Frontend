99ae1ede02ee40840023b6b6ff82a37a1cd2366c
1snajper <tuleckikacper@gmail.com>
2024-06-14 21:34:01 +0200

    Added href for creating teams

--- front/src/Component/Admin/NavBarAdmin.js
+++ front/src/Component/Admin/NavBarAdmin.js
@@ -1,6 +1,9 @@
 import '../../Style/Admin/NavBarAdmin.css'
 
 const NavBarAdmin = () => {
+    const gotoCreateTeam =()=>{
+        window.location.href='/createteam/'
+    }
     return ( 
         <div id="navbaradminmain">
             <div id="navPaneAdmin">
@@ -8,7 +11,7 @@ const NavBarAdmin = () => {
                     <i className="icon-user"/>
                 </div>
                 <div className="navbarbuttons">
-                    <i className="icon-plus"/>
+                    <i className="icon-plus" onClick={gotoCreateTeam}/>
                 </div>
             </div>
         </div>
