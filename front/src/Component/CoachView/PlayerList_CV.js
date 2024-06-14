e16edda51ed7a44c36ffbb6e29f0a54cd9ee75bd
Arroyo75 <jszczepanski75@gmail.com>
2024-06-14 22:05:06 +0200

    Player_List_Quick_Fix

--- front/src/Component/CoachView/PlayerList_CV.js
+++ front/src/Component/CoachView/PlayerList_CV.js
@@ -31,25 +31,36 @@ const PlayerList_CV = ({ onSelectPlayer,sharedState }) => {
     useEffect(() => {
         fetchPlayerList_CV()
     }, [sharedState])
-    // return(
-    //     <div id="playerList_CV">
-    //         <div id = "listOfPlayers_CV">
-    //         {fetchedPlayers.map(player => (
-    //             <div key={player.id} onClick={() => onSelectPlayer(player.id)} className="playerCard_CV">
-    //                 <img src={'https://i.pravatar.cc/100?img=1'} alt={`${player.first_name} ${player.lastName}`} />
-    //                 <span className="playerName_CV bStyle">{`${player.first_name} ${player.last_name} ${player.player.player_positions.map(position => position.position.position_code)}`}</span>
-    //             </div>
-    //         ))}
-    //         </div>
-    //     </div>
-    // );
+
+    const handleCopyClick = async () => {
+        try {
+            const response = await fetch('YOUR_BACKEND_ENDPOINT');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            // Assume the link is returned in the `link` field of the response data
+            const link = data.link;
+
+            // Handle copying the link to the stash here (e.g., using the Clipboard API)
+            navigator.clipboard.writeText(link).then(() => {
+                window.alert('Link copied successfully: ' +{link});
+            });
+        } catch (error) {
+            console.error('Error fetching link:', error);
+            setAlertMessage('Failed to copy link.');
+            setShowAlert(true);
+            setTimeout(() => setShowAlert(false), 3000); // Hide alert after 3 seconds
+        }
+        window.alert("asdasddasd")
+    };
 
     return (
         <div id="playerList_CV">
             <div className="addPlayerCard_CV">
                 <i class="icon-plus-alternative"/>
                 <span className="playerName_CV bStyle">DODAJ ZAWODNIKA</span>
-                <button className="copy_CV" >Kopiuj</button>
+                <button className="copy_CV" onClick={handleCopyClick}>Kopiuj</button>
             </div>
             <div id = "listOfPlayers_CV">
             {fetchedPlayers.map(player => (
