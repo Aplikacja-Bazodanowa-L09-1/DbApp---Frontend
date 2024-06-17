251a1c8a234d374bd0b1d02ecadfb1251f607da1
Nevixsus <150295926+Nevixsus@users.noreply.github.com>
2024-06-17 20:33:28 +0200

    Added fetch (not working) RentedEquipmentPane for coach

--- front/src/Component/CoachView/RentedEquipmentPane_CV.js
+++ front/src/Component/CoachView/RentedEquipmentPane_CV.js
@@ -4,22 +4,49 @@ import "../../Style/CoachView/RentedEquipmentPane_CV.css";
 const RentedEquipmentPane_CV = ({ playerId }) => {
     const [rEquipment, setREquipment] = useState([]);
 
-    useEffect(() => {
-        const fetchRentedEquipment = async (playerId) => {
-            try {
-                const response = await fetch(`YOUR_API_ENDPOINT/${playerId}/rented-equipment`);
-                const data = await response.json();
-                setREquipment(data);
-            } catch (error) {
-                console.error('Error fetching rented equipment:', error);
+    // useEffect(() => {
+    //     const fetchRentedEquipment = async (playerId) => {
+    //         try {
+    //             const response = await fetch(`YOUR_API_ENDPOINT/${playerId}/rented-equipment`);
+    //             const data = await response.json();
+    //             setREquipment(data);
+    //         } catch (error) {
+    //             console.error('Error fetching rented equipment:', error);
+    //         }
+    //     };
+
+    //     if (playerId) {
+    //         fetchRentedEquipment(playerId);
+    //     }
+    // }, [playerId]);
+
+    const fetchRentedEquipment_CV = (playerId) => {
+        fetch(`${process.env.REACT_APP_SERVER_ADDRESS}/coach/equipment/rented?playerId=${playerId}`, {
+            mode: 'cors',
+            method: 'GET',
+            headers: { "Content-Type": "application/json", "authorization": `Berear ${localStorage.getItem('access_token')}` },
+        }).then(response => response.json()).then(data => {
+            if (data.detail) {
+                console.log(data.detail)
+            } else {
+
+                //console.log(data.eq)
+                // for(let i=0; i<data.eq.length; i++){
+                //     console.log(data.eq[i])
+                //     setAEquipment([])
+                // }
+                setREquipment(data)
+                console.log('kk')
             }
-        };
+        })
+    }
 
-        if (playerId) {
-            fetchRentedEquipment(playerId);
+    useEffect(() => {
+        if(playerId){
+            fetchRentedEquipment_CV(playerId)
         }
-    }, [playerId]);
-
+    }, [playerId])
+    
     //from here
     let placeHolder1 = null;
 
